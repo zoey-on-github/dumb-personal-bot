6ccee16e7af9fbae65d7f1b402292898c3247c62
zoey-on-github <supercoolguy56911@gmail.com>
2022-01-22 23:48:59 -0800

    made a fuckton of changes, added a bunch of files, and added some bot commands

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ client.once('ready', () => {
 
 // Login to Discord with your client's token
 client.login(token);
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+}); 

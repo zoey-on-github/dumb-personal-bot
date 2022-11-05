b4067cd2951cedba6607f0bbb160b8a760aa8ef5
julie <8806187+zoey-on-github@users.noreply.github.com>
2022-11-05 00:51:20 -0700

    fixed (most) commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,8 @@
 //deploys guild commands, and I should note: THIS ONLY WORKS ON THE GUILD YOU ASSIGN IT TO, if you want global commands, USE THE GLOBAL COMMANDS SCRIPT
-const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
+const fs = require('node:fs');
+const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
+const path = require('node:path');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path')
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -16,20 +16,27 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+	if ('data' in command && 'execute' in command){ 
+	client.commands.set(command.data.name, command); 
+	} else {
+		console.log(`[WARNING] the command from ${filePath} is missing a required property -w-`)
+	}
 }
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once(Events.ClientReady, c  => {
+	console.log(`Ready!, logged in as ${c.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+	const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) return;
-
+	if (!command)
+	{
+		console.error(`no command matching ${interaction.commandName} could be found ;w;`);
+	return;
+	}
 	try {
 		await command.execute(interaction);
 	} catch (error) {

a00857c0823f87669b979e44e1d52f13edd6c906
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-05-12 23:38:12 -0700

    doesn't work rn :P

--- making-the-bot-talk.js
+++ making-the-bot-talk.js
@@ -1,9 +1,10 @@
-const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+//const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const channel = client.channels.cache.get("952081516010303528");
 
-channel.send("<!@490681810548948992> > about time you dumb robot fox \n not cool zeich ;w;");
+channel.send("test");
 
 // Login to Discord with your client's token
 client.login(token);
